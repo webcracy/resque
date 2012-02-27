d4ec9f0907c660e30ada764fd3cfac19e2c241b8
Alexandre Loureiro Solleiro <alex@webcracy.org>
2012-02-27 17:33:27 +0000

    ditto

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -51,9 +51,10 @@ namespace :resque do
   task :preload => :setup do
     if defined?(Rails) && Rails.respond_to?(:application)
       # Rails 3
-      Rails.application.eager_load!
-      Rails.application.railties.engines.map { |e| e.eager_load! }
-      all_models = ActiveRecord::Base.subclasses
+      # Rails.application.eager_load!
+      # Rails.application.railties.engines.map { |e| e.eager_load! }
+      # all_models = ActiveRecord::Base.subclasses
+      Rake::Task[:environment].invoke
     elsif defined?(Rails::Initializer)
       # Rails 2.3
       $rails_rake_task = false

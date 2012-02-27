081032bab2d3ac0a1c3f46e814b2a481dd19faa4
Alexandre Loureiro Solleiro <alex@webcracy.org>
2012-02-27 17:25:48 +0000

    Trying to fix unknown constant error when loading an Engine

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -51,7 +51,8 @@ namespace :resque do
   task :preload => :setup do
     if defined?(Rails) && Rails.respond_to?(:application)
       # Rails 3
-      Rails.application.eager_load!
+      Rails.application.railties.engines.map { |e| e.eager_load! }
+      # Rails.application.eager_load!
     elsif defined?(Rails::Initializer)
       # Rails 2.3
       $rails_rake_task = false
